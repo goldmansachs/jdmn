a40d97bcc49ed47109a3135ddfebe1171621b23c
opatrascoiu <opatrascoiu@yahoo.com>
2018-03-02 10:41:03 +0000

    [#54] Remove redundant CL2 tests from dmn-core

--- dmn-core/src/test/java/com/gs/dmn/transformation/CL3TckDMNToJavaTransformerTest.java
+++ dmn-core/src/test/java/com/gs/dmn/transformation/CL3TckDMNToJavaTransformerTest.java
@@ -27,18 +27,9 @@ public class CL3TckDMNToJavaTransformerTest extends AbstractTckDMNToJavaTransfor
 
     @Test
     public void testCL3() throws Exception {
-        doTest("0001-filter");
-        doTest("0002-string-functions");
-        doTest("0003-iteration");
-        doTest("0005-literal-invocation");
         doTest("0004-lending");
+        doTest("0005-literal-invocation");
         doTest("0006-join");
-        doTest("0007-date-time");
-        doTest("0008-listGen");
-        doTest("0009-append-flatten");
-        doTest("0010-concatenate");
-        doTest("0011-insert-remove");
-        doTest("0012-list-functions");
         doTest("0013-sort");
         doTest("0014-loan-comparison");
         doTest("0016-some-every");
@@ -47,19 +38,6 @@ public class CL3TckDMNToJavaTransformerTest extends AbstractTckDMNToJavaTransfor
         doTest("0021-singleton-list");
         doTest("0030-user-defined-functions");
         doTest("0031-user-defined-functions");
-        doTest("0032-conditionals");
-        doTest("0033-for-loops");
-        doTest("1100-feel-decimal-function");
-        doTest("1101-feel-floor-function");
-        doTest("1102-feel-ceiling-function");
-        doTest("1103-feel-substring-function");
-        doTest("1104-feel-string-length-function");
-        doTest("1105-feel-upper-case-function");
-        doTest("1106-feel-lower-case-function");
-        doTest("1107-feel-substring-before-function");
-        doTest("1108-feel-substring-after-function");
-        doTest("1109-feel-replace-function");
-        doTest("1110-feel-contains-function");
 
         doTest("9001-recursive-function");
     }
