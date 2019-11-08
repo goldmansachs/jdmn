fcdb44d756fac179405e2a0ae8a735770894836d
Patrascoiu, Octavian [Tech] <octavian.patrascoiu@ln.email.gs.com>
2019-11-08 17:44:19 +0000

    Add dmn-signavio-integration-tests

---
+++ dmn-signavio-integration-tests/src/test/java/com/gs/dmn/simple_decision_primitive_type_inputs_feel_input_entries_single_output_first_hit_policy/HandwrittenDecisionTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.gs.dmn.simple_decision_primitive_type_inputs_feel_input_entries_single_output_first_hit_policy;
+
+import com.gs.dmn.AbstractHandwrittenDecisionTest;
+import com.gs.dmn.runtime.annotation.AnnotationSet;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HandwrittenDecisionTest extends AbstractHandwrittenDecisionTest {
+    private final Decision decision = new Decision();
+
+    @Test
+    public void testApply() {
+        String result = applyDecision("true", "2016-08-01T11:00:00Z", "2016-08-01", "e1", "-1", "abc", "12:00:00Z");
+        assertEquals("r1", result);
+    }
+
+    private String applyDecision(
+            String booleanString, String dateAndTimeString, String dateString, String enumerationString,
+            String numberString, String text, String timeString) {
+
+        AnnotationSet annotationSet = new AnnotationSet();
+
+        return decision.apply(booleanString, dateAndTimeString, dateString, enumerationString, numberString, text, timeString, annotationSet);
+    }
+
+    @Override
+    protected void applyDecision() {
+        AnnotationSet annotationSet = new AnnotationSet();
+        String dateInput = "2016-08-01";
+        String timeInput = "12:00:00Z";
+        String dateAndTimeInput = "2016-08-01T11:00:00Z";
+        String numberInput = "-1";
+        String textInput = "abc";
+        String booleanInput = "true";
+        String enumerationInput = "e1";
+
+        decision.apply(booleanInput, dateAndTimeInput, dateInput, enumerationInput, numberInput, textInput, timeInput, annotationSet);
+    }
+}
---
+++ dmn-signavio-integration-tests/src/test/java/com/gs/dmn/type_interface_deserialization/HandwrittenDecisionTest.java
@@ -0,0 +1,46 @@
+package com.gs.dmn.type_interface_deserialization;
+
+import com.gs.dmn.AbstractHandwrittenDecisionTest;
+import org.junit.Assert;
+
+
+public class HandwrittenDecisionTest extends AbstractHandwrittenDecisionTest {
+
+    private final EsmaEquityOptionIndicator decision = new EsmaEquityOptionIndicator();
+
+    @org.junit.Test
+    public void testDeserializedDecisionInput() throws Exception {
+        com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_ = new com.gs.dmn.runtime.annotation.AnnotationSet();
+        com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfiles marginEquityOptionTradableProfiles = new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfilesImpl(decision.asList(new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfileImpl(Boolean.TRUE, "abc")));
+
+        String esmaEquityOptionIndicator = decision.apply(marginEquityOptionTradableProfiles, annotationSet_);
+
+        checkValues("Yes", esmaEquityOptionIndicator);
+    }
+
+    @org.junit.Test
+    public void testSerializedDecisionInput() throws Exception {
+        com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_ = new com.gs.dmn.runtime.annotation.AnnotationSet();
+
+        com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfiles marginEquityOptionTradableProfiles = new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfilesImpl(decision.asList(new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfileImpl(Boolean.TRUE, "abc")));
+        String serializedInput = com.gs.dmn.serialization.JsonSerializer.OBJECT_MAPPER.writeValueAsString(marginEquityOptionTradableProfiles);
+
+        String esmaEquityOptionIndicator = decision.apply(serializedInput, annotationSet_);
+
+        checkValues("Yes", esmaEquityOptionIndicator);
+    }
+
+
+    @Override
+    protected void applyDecision() {
+        com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_ = new com.gs.dmn.runtime.annotation.AnnotationSet();
+        com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfiles marginEquityOptionTradableProfiles = new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfilesImpl(decision.asList(new com.gs.dmn.type_interface_deserialization.type.MarginEquityOptionTradableProfileImpl(Boolean.TRUE, "abc")));
+
+        decision.apply(marginEquityOptionTradableProfiles, annotationSet_);
+    }
+
+    private void checkValues(Object expected, Object actual) {
+        Assert.assertEquals(expected == null ? null : expected.toString(), actual == null ? null : actual.toString());
+    }
+
+}
