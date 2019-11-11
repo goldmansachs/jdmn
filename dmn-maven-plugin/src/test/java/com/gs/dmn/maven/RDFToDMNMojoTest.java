7fd08c6be1c78925fe336bfcde4ba32822026b7d
opatrascoiu <opatrascoiu@yahoo.com>
2019-11-11 16:29:40 +0000

    [#233] Open source the signavio module. Add tests for mojos.

---
+++ dmn-maven-plugin/src/test/java/com/gs/dmn/maven/RDFToDMNMojoTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.gs.dmn.maven;
+
+import org.apache.maven.project.MavenProject;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertTrue;
+
+public class RDFToDMNMojoTest extends AbstractMojoTest {
+    private final RDFToDMNMojo mojo = new RDFToDMNMojo();
+    private final MavenProject project = new MavenProject();
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExecuteWhenMissingInput() throws Exception {
+        mojo.inputParameters = makeParams();
+        mojo.execute();
+        assertTrue(true);
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        String input = this.getClass().getClassLoader().getResource("input/NPEValidation2.xml").getFile();
+        mojo.project = project;
+        mojo.inputFileDirectory = new File(input);
+        mojo.outputFileDirectory = new File("target/output");
+        mojo.inputParameters = makeParams();
+
+        mojo.execute();
+        assertTrue(true);
+    }
+
+    @Override
+    protected Map<String, String> makeParams() {
+        Map<String, String> params = new LinkedHashMap<>();
+        params.put("namespace", "http://wwww.org.com/cip");
+        params.put("prefix", "cip");
+        return params;
+    }
+}
