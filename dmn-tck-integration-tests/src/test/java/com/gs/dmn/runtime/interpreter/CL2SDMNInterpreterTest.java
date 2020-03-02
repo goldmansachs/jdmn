3fb42f4ef98415dce784b9f24c6bd1d438026491
opatrascoiu <opatrascoiu@yahoo.com>
2020-03-02 13:15:27 +0000

    [#270] Import. Rename test methods to match the ones used when testing the transformer

--- dmn-tck-integration-tests/src/test/java/com/gs/dmn/runtime/interpreter/CL2SDMNInterpreterTest.java
+++ dmn-tck-integration-tests/src/test/java/com/gs/dmn/runtime/interpreter/CL2SDMNInterpreterTest.java
@@ -27,33 +27,33 @@ public abstract class CL2SDMNInterpreterTest extends AbstractDMNInterpreterTest 
 
     @Test
     public void testCL2() {
-        doTestDiagram("0001-input-data-string");
-        doTestDiagram("0002-input-data-number");
-        doTestDiagram("0003-input-data-string-allowed-values");
-        doTestDiagram("0004-simpletable-U");
-        doTestDiagram("0005-simpletable-A");
-        doTestDiagram("0006-simpletable-P1");
-        doTestDiagram("0007-simpletable-P2");
-        doTestDiagram("0008-LX-arithmetic");
-        doTestDiagram("0009-invocation-arithmetic");
-        doTestDiagram("0010-multi-output-U");
-        doTestDiagram("0100-feel-constants");
-        doTestDiagram("0101-feel-constants");
-        doTestDiagram("0102-feel-constants");
-        doTestDiagram("0105-feel-math");
-        doTestDiagram("0106-feel-ternary-logic");
-        doTestDiagram("0107-feel-ternary-logic-not");
-        doTestDiagram("0108-first-hitpolicy");
-        doTestDiagram("0109-ruleOrder-hitpolicy");
-        doTestDiagram("0110-outputOrder-hitpolicy");
-        doTestDiagram("0111-first-hitpolicy-singleoutputcol");
-        doTestDiagram("0112-ruleOrder-hitpolicy-singleinoutcol");
-        doTestDiagram("0113-outputOrder-hitpolicy-singleinoutcol");
-        doTestDiagram("0114-min-collect-hitpolicy");
-        doTestDiagram("0115-sum-collect-hitpolicy");
-        doTestDiagram("0116-count-collect-hitpolicy");
-        doTestDiagram("0117-multi-any-hitpolicy");
-        doTestDiagram("0118-multi-priority-hitpolicy");
-        doTestDiagram("0119-multi-collect-hitpolicy");
+        doSingleModelTest("0001-input-data-string");
+        doSingleModelTest("0002-input-data-number");
+        doSingleModelTest("0003-input-data-string-allowed-values");
+        doSingleModelTest("0004-simpletable-U");
+        doSingleModelTest("0005-simpletable-A");
+        doSingleModelTest("0006-simpletable-P1");
+        doSingleModelTest("0007-simpletable-P2");
+        doSingleModelTest("0008-LX-arithmetic");
+        doSingleModelTest("0009-invocation-arithmetic");
+        doSingleModelTest("0010-multi-output-U");
+        doSingleModelTest("0100-feel-constants");
+        doSingleModelTest("0101-feel-constants");
+        doSingleModelTest("0102-feel-constants");
+        doSingleModelTest("0105-feel-math");
+        doSingleModelTest("0106-feel-ternary-logic");
+        doSingleModelTest("0107-feel-ternary-logic-not");
+        doSingleModelTest("0108-first-hitpolicy");
+        doSingleModelTest("0109-ruleOrder-hitpolicy");
+        doSingleModelTest("0110-outputOrder-hitpolicy");
+        doSingleModelTest("0111-first-hitpolicy-singleoutputcol");
+        doSingleModelTest("0112-ruleOrder-hitpolicy-singleinoutcol");
+        doSingleModelTest("0113-outputOrder-hitpolicy-singleinoutcol");
+        doSingleModelTest("0114-min-collect-hitpolicy");
+        doSingleModelTest("0115-sum-collect-hitpolicy");
+        doSingleModelTest("0116-count-collect-hitpolicy");
+        doSingleModelTest("0117-multi-any-hitpolicy");
+        doSingleModelTest("0118-multi-priority-hitpolicy");
+        doSingleModelTest("0119-multi-collect-hitpolicy");
     }
 }
