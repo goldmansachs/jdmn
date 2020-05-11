6d6fbca7e9d03c1b2e6e59218f40b01cd1b0844e
opatrascoiu <opatrascoiu@yahoo.com>
2020-05-11 19:42:13 +0100

    [#277] Kotlin: Add transformer for DMN TCK to Kotlin

---
+++ dmn-core/src/test/resources/tck/cl2/expected/kotlin/test/0006-simpletable-p1/Test0006SimpletableP1.kt
@@ -0,0 +1,52 @@
+
+import java.util.*
+import java.util.stream.Collectors
+
+@javax.annotation.Generated(value = ["junit.ftl", "0006-simpletable-P1.dmn"])
+class Test0006SimpletableP1 : com.gs.dmn.runtime.DefaultDMNBaseDecision {
+    @org.junit.Test
+    fun testCase001() {
+        val annotationSet_ = com.gs.dmn.runtime.annotation.AnnotationSet()
+        val eventListener_ = com.gs.dmn.runtime.listener.NopEventListener()
+        val externalExecutor_ = com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor()
+        // Initialize input data
+        val age: java.math.BigDecimal = number("18")
+        val riskCategory: String = "Medium"
+        val isAffordable: Boolean = true
+
+        // Check ApprovalStatus
+        checkValues("Approved", ApprovalStatus().apply(age, riskCategory, isAffordable, annotationSet_, eventListener_, externalExecutor_))
+    }
+
+    @org.junit.Test
+    fun testCase002() {
+        val annotationSet_ = com.gs.dmn.runtime.annotation.AnnotationSet()
+        val eventListener_ = com.gs.dmn.runtime.listener.NopEventListener()
+        val externalExecutor_ = com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor()
+        // Initialize input data
+        val age: java.math.BigDecimal = number("17")
+        val riskCategory: String = "Medium"
+        val isAffordable: Boolean = true
+
+        // Check ApprovalStatus
+        checkValues("Declined", ApprovalStatus().apply(age, riskCategory, isAffordable, annotationSet_, eventListener_, externalExecutor_))
+    }
+
+    @org.junit.Test
+    fun testCase003() {
+        val annotationSet_ = com.gs.dmn.runtime.annotation.AnnotationSet()
+        val eventListener_ = com.gs.dmn.runtime.listener.NopEventListener()
+        val externalExecutor_ = com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor()
+        // Initialize input data
+        val age: java.math.BigDecimal = number("18")
+        val riskCategory: String = "High"
+        val isAffordable: Boolean = true
+
+        // Check ApprovalStatus
+        checkValues("Declined", ApprovalStatus().apply(age, riskCategory, isAffordable, annotationSet_, eventListener_, externalExecutor_))
+    }
+
+    private fun checkValues(Object expected, Object actual) {
+        com.gs.dmn.runtime.Assert.assertEquals(expected, actual)
+    }
+}
