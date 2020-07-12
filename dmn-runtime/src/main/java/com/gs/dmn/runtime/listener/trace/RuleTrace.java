fa5bcd5a36e82e3f17ed5ae8ae2c6950b61c9aea
opatrascoiu <opatrascoiu@yahoo.com>
2020-07-12 16:57:29 +0100

    [#324] Trace listener: Added trace listener for existing listener API

---
+++ dmn-runtime/src/main/java/com/gs/dmn/runtime/listener/trace/RuleTrace.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.gs.dmn.runtime.listener.trace;
+
+import com.gs.dmn.runtime.listener.Rule;
+
+public class RuleTrace {
+    private final Rule rule;
+    private boolean matched;
+    private Object result;
+
+    public RuleTrace(Rule rule) {
+        this.rule = rule;
+    }
+
+    public Rule getRule() {
+        return rule;
+    }
+
+    public boolean isMatched() {
+        return matched;
+    }
+
+    public void setMatched(boolean matched) {
+        this.matched = matched;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+}
