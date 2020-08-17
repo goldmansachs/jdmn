5a27f7e77c504939f97eb8bd5168b886f5dfd4ab
opatrascoiu <opatrascoiu@yahoo.com>
2020-08-17 13:49:37 +0100

    [#325] gRPC: Added initial Kotlin tests for Signavio

---
+++ dmn-signavio/src/test/java/com/gs/dmn/signavio/testlab/TestLabToKotlinProtoTransformerTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 Goldman Sachs.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package com.gs.dmn.signavio.testlab;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class TestLabToKotlinProtoTransformerTest extends AbstractTestLabToJavaJUnitTransformerTest {
+    @Test
+    public void testExampleCreditDecision() throws Exception {
+        doTest("Example credit decision");
+    }
+
+    @Override
+    protected String getInputPath() {
+        return "dmn2java/exported/complex/input";
+    }
+
+    @Override
+    protected String getExpectedPath() {
+        return "dmn2java/exported/proto/expected/proto3/kotlin/test-lab";
+    }
+
+    @Override
+    protected Map<String, String> makeInputParameters() {
+        Map<String, String> inputParams = super.makeInputParameters();
+        inputParams.put("generateProtoMessages", "true");
+        inputParams.put("generateProtoServices", "true");
+        inputParams.put("protoVersion", "proto3");
+        return inputParams;
+    }
+}
---
+++ dmn-signavio/src/test/resources/dmn2java/exported/proto/expected/proto3/kotlin/dmn/example-credit-decision/ProcessPriorIssues.java
@@ -0,0 +1,264 @@
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@javax.annotation.Generated(value = {"signavio-decision.ftl", "processPriorIssues"})
+@com.gs.dmn.runtime.annotation.DRGElement(
+    namespace = "",
+    name = "processPriorIssues",
+    label = "Process prior issues",
+    elementKind = com.gs.dmn.runtime.annotation.DRGElementKind.DECISION,
+    expressionKind = com.gs.dmn.runtime.annotation.ExpressionKind.DECISION_TABLE,
+    hitPolicy = com.gs.dmn.runtime.annotation.HitPolicy.COLLECT,
+    rulesCount = 5
+)
+public class ProcessPriorIssues extends com.gs.dmn.signavio.runtime.DefaultSignavioBaseDecision {
+    public static final com.gs.dmn.runtime.listener.DRGElement DRG_ELEMENT_METADATA = new com.gs.dmn.runtime.listener.DRGElement(
+        "",
+        "processPriorIssues",
+        "Process prior issues",
+        com.gs.dmn.runtime.annotation.DRGElementKind.DECISION,
+        com.gs.dmn.runtime.annotation.ExpressionKind.DECISION_TABLE,
+        com.gs.dmn.runtime.annotation.HitPolicy.COLLECT,
+        5
+    );
+
+    public ProcessPriorIssues() {
+    }
+
+    public List<java.math.BigDecimal> apply(String applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_) {
+        try {
+            return apply((applicant != null ? com.gs.dmn.serialization.JsonSerializer.OBJECT_MAPPER.readValue(applicant, new com.fasterxml.jackson.core.type.TypeReference<type.ApplicantImpl>() {}) : null), annotationSet_, new com.gs.dmn.runtime.listener.LoggingEventListener(LOGGER), new com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor());
+        } catch (Exception e) {
+            logError("Cannot apply decision 'ProcessPriorIssues'", e);
+            return null;
+        }
+    }
+
+    public List<java.math.BigDecimal> apply(String applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        try {
+            return apply((applicant != null ? com.gs.dmn.serialization.JsonSerializer.OBJECT_MAPPER.readValue(applicant, new com.fasterxml.jackson.core.type.TypeReference<type.ApplicantImpl>() {}) : null), annotationSet_, eventListener_, externalExecutor_);
+        } catch (Exception e) {
+            logError("Cannot apply decision 'ProcessPriorIssues'", e);
+            return null;
+        }
+    }
+
+    public List<java.math.BigDecimal> apply(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_) {
+        return apply(applicant, annotationSet_, new com.gs.dmn.runtime.listener.LoggingEventListener(LOGGER), new com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor());
+    }
+
+    public List<java.math.BigDecimal> apply(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        try {
+            // Start decision 'processPriorIssues'
+            long processPriorIssuesStartTime_ = System.currentTimeMillis();
+            com.gs.dmn.runtime.listener.Arguments processPriorIssuesArguments_ = new com.gs.dmn.runtime.listener.Arguments();
+            processPriorIssuesArguments_.put("Applicant", applicant);
+            eventListener_.startDRGElement(DRG_ELEMENT_METADATA, processPriorIssuesArguments_);
+
+            // Evaluate decision 'processPriorIssues'
+            List<java.math.BigDecimal> output_ = evaluate(applicant, annotationSet_, eventListener_, externalExecutor_);
+
+            // End decision 'processPriorIssues'
+            eventListener_.endDRGElement(DRG_ELEMENT_METADATA, processPriorIssuesArguments_, output_, (System.currentTimeMillis() - processPriorIssuesStartTime_));
+
+            return output_;
+        } catch (Exception e) {
+            logError("Exception caught in 'processPriorIssues' evaluation", e);
+            return null;
+        }
+    }
+
+    public proto.ProcessPriorIssuesResponse apply(proto.ProcessPriorIssuesRequest processPriorIssuesRequest_, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_) {
+        return apply(processPriorIssuesRequest_, annotationSet_, new com.gs.dmn.runtime.listener.LoggingEventListener(LOGGER), new com.gs.dmn.runtime.external.DefaultExternalFunctionExecutor());
+    }
+
+    public proto.ProcessPriorIssuesResponse apply(proto.ProcessPriorIssuesRequest processPriorIssuesRequest_, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Create arguments from Request Message
+        type.Applicant applicant = type.Applicant.toApplicant(processPriorIssuesRequest_.getApplicant());
+        
+        // Invoke apply method
+        List<java.math.BigDecimal> output_ = apply(applicant, annotationSet_, eventListener_, externalExecutor_);
+        
+        // Convert output to Response Message
+        proto.ProcessPriorIssuesResponse.Builder builder_ = proto.ProcessPriorIssuesResponse.newBuilder();
+        builder_.addAllProcessPriorIssues(((List) output_.stream().map(e -> (e == null ? 0 : e.doubleValue())).collect(java.util.stream.Collectors.toList())));
+        return builder_.build();
+    }
+
+    protected List<java.math.BigDecimal> evaluate(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Apply rules and collect results
+        com.gs.dmn.runtime.RuleOutputList ruleOutputList_ = new com.gs.dmn.runtime.RuleOutputList();
+        ruleOutputList_.add(rule0(applicant, annotationSet_, eventListener_, externalExecutor_));
+        ruleOutputList_.add(rule1(applicant, annotationSet_, eventListener_, externalExecutor_));
+        ruleOutputList_.add(rule2(applicant, annotationSet_, eventListener_, externalExecutor_));
+        ruleOutputList_.add(rule3(applicant, annotationSet_, eventListener_, externalExecutor_));
+        ruleOutputList_.add(rule4(applicant, annotationSet_, eventListener_, externalExecutor_));
+
+        // Return results based on hit policy
+        List<java.math.BigDecimal> output_;
+        if (ruleOutputList_.noMatchedRules()) {
+            // Default value
+            output_ = null;
+            if (output_ == null) {
+                output_ = asList();
+            }
+        } else {
+            List<? extends com.gs.dmn.runtime.RuleOutput> ruleOutputs_ = ruleOutputList_.applyMultiple(com.gs.dmn.runtime.annotation.HitPolicy.COLLECT);
+            output_ = ruleOutputs_.stream().map(o -> ((ProcessPriorIssuesRuleOutput)o).getProcessPriorIssues()).collect(Collectors.toList());
+        }
+
+        return output_;
+    }
+
+    @com.gs.dmn.runtime.annotation.Rule(index = 0, annotation = "\"\"")
+    public com.gs.dmn.runtime.RuleOutput rule0(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Rule metadata
+        com.gs.dmn.runtime.listener.Rule drgRuleMetadata = new com.gs.dmn.runtime.listener.Rule(0, "\"\"");
+
+        // Rule start
+        eventListener_.startRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+        // Apply rule
+        ProcessPriorIssuesRuleOutput output_ = new ProcessPriorIssuesRuleOutput(false);
+        if (ruleMatches(eventListener_, drgRuleMetadata,
+            booleanNot((notContainsAny(((List<String>)(applicant != null ? applicant.getPriorIssues() : null)), asList("Card rejection", "Late payment"))))
+        )) {
+            // Rule match
+            eventListener_.matchRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+            // Compute output
+            output_.setMatched(true);
+            output_.setProcessPriorIssues(numericUnaryMinus(number("10")));
+
+            // Add annotation
+            annotationSet_.addAnnotation("processPriorIssues", 0, "");
+        }
+
+        // Rule end
+        eventListener_.endRule(DRG_ELEMENT_METADATA, drgRuleMetadata, output_);
+
+        return output_;
+    }
+
+    @com.gs.dmn.runtime.annotation.Rule(index = 1, annotation = "\"\"")
+    public com.gs.dmn.runtime.RuleOutput rule1(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Rule metadata
+        com.gs.dmn.runtime.listener.Rule drgRuleMetadata = new com.gs.dmn.runtime.listener.Rule(1, "\"\"");
+
+        // Rule start
+        eventListener_.startRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+        // Apply rule
+        ProcessPriorIssuesRuleOutput output_ = new ProcessPriorIssuesRuleOutput(false);
+        if (ruleMatches(eventListener_, drgRuleMetadata,
+            booleanNot((notContainsAny(((List<String>)(applicant != null ? applicant.getPriorIssues() : null)), asList("Default on obligations"))))
+        )) {
+            // Rule match
+            eventListener_.matchRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+            // Compute output
+            output_.setMatched(true);
+            output_.setProcessPriorIssues(numericUnaryMinus(number("30")));
+
+            // Add annotation
+            annotationSet_.addAnnotation("processPriorIssues", 1, "");
+        }
+
+        // Rule end
+        eventListener_.endRule(DRG_ELEMENT_METADATA, drgRuleMetadata, output_);
+
+        return output_;
+    }
+
+    @com.gs.dmn.runtime.annotation.Rule(index = 2, annotation = "\"\"")
+    public com.gs.dmn.runtime.RuleOutput rule2(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Rule metadata
+        com.gs.dmn.runtime.listener.Rule drgRuleMetadata = new com.gs.dmn.runtime.listener.Rule(2, "\"\"");
+
+        // Rule start
+        eventListener_.startRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+        // Apply rule
+        ProcessPriorIssuesRuleOutput output_ = new ProcessPriorIssuesRuleOutput(false);
+        if (ruleMatches(eventListener_, drgRuleMetadata,
+            booleanNot((notContainsAny(((List<String>)(applicant != null ? applicant.getPriorIssues() : null)), asList("Bankruptcy"))))
+        )) {
+            // Rule match
+            eventListener_.matchRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+            // Compute output
+            output_.setMatched(true);
+            output_.setProcessPriorIssues(numericUnaryMinus(number("100")));
+
+            // Add annotation
+            annotationSet_.addAnnotation("processPriorIssues", 2, "");
+        }
+
+        // Rule end
+        eventListener_.endRule(DRG_ELEMENT_METADATA, drgRuleMetadata, output_);
+
+        return output_;
+    }
+
+    @com.gs.dmn.runtime.annotation.Rule(index = 3, annotation = "\"\"")
+    public com.gs.dmn.runtime.RuleOutput rule3(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Rule metadata
+        com.gs.dmn.runtime.listener.Rule drgRuleMetadata = new com.gs.dmn.runtime.listener.Rule(3, "\"\"");
+
+        // Rule start
+        eventListener_.startRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+        // Apply rule
+        ProcessPriorIssuesRuleOutput output_ = new ProcessPriorIssuesRuleOutput(false);
+        if (ruleMatches(eventListener_, drgRuleMetadata,
+            (notContainsAny(((List<String>)(applicant != null ? applicant.getPriorIssues() : null)), asList("Card rejection", "Late payment", "Default on obligations", "Bankruptcy")))
+        )) {
+            // Rule match
+            eventListener_.matchRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+            // Compute output
+            output_.setMatched(true);
+            output_.setProcessPriorIssues(number("50"));
+
+            // Add annotation
+            annotationSet_.addAnnotation("processPriorIssues", 3, "");
+        }
+
+        // Rule end
+        eventListener_.endRule(DRG_ELEMENT_METADATA, drgRuleMetadata, output_);
+
+        return output_;
+    }
+
+    @com.gs.dmn.runtime.annotation.Rule(index = 4, annotation = "\"\"")
+    public com.gs.dmn.runtime.RuleOutput rule4(type.Applicant applicant, com.gs.dmn.runtime.annotation.AnnotationSet annotationSet_, com.gs.dmn.runtime.listener.EventListener eventListener_, com.gs.dmn.runtime.external.ExternalFunctionExecutor externalExecutor_) {
+        // Rule metadata
+        com.gs.dmn.runtime.listener.Rule drgRuleMetadata = new com.gs.dmn.runtime.listener.Rule(4, "\"\"");
+
+        // Rule start
+        eventListener_.startRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+        // Apply rule
+        ProcessPriorIssuesRuleOutput output_ = new ProcessPriorIssuesRuleOutput(false);
+        if (ruleMatches(eventListener_, drgRuleMetadata,
+            Boolean.TRUE
+        )) {
+            // Rule match
+            eventListener_.matchRule(DRG_ELEMENT_METADATA, drgRuleMetadata);
+
+            // Compute output
+            output_.setMatched(true);
+            output_.setProcessPriorIssues(numericMultiply(count(((List<String>)(applicant != null ? applicant.getPriorIssues() : null))), numericUnaryMinus(number("5"))));
+
+            // Add annotation
+            annotationSet_.addAnnotation("processPriorIssues", 4, "");
+        }
+
+        // Rule end
+        eventListener_.endRule(DRG_ELEMENT_METADATA, drgRuleMetadata, output_);
+
+        return output_;
+    }
+
+}
