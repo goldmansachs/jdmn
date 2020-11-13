cd50309ec3041f0618fd9ed2d02728f4299b7116
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2020-11-13 16:46:49 +0000

    Break interpreter tests in independent units

--- dmn-tck-it/dmn-tck-it-interpreter/src/test/java/com/gs/dmn/runtime/interpreter/CL3DMNInterpreterTest.java
+++ dmn-tck-it/dmn-tck-it-interpreter/src/test/java/com/gs/dmn/runtime/interpreter/CL3DMNInterpreterTest.java
@@ -24,69 +24,292 @@ public abstract class CL3DMNInterpreterTest<NUMBER, DATE, TIME, DATE_TIME, DURAT
     }
 
     @Test
-    public void testCL3() {
+    public void test_11_cl3_0001_filter() {
         // DMN 1.1 files
         doSingleModelTest("0001-filter");
+    }
+
+    @Test
+    public void test_11_cl3_0002_string_functions() {
         doSingleModelTest("0002-string-functions");
+    }
+
+    @Test
+    public void test_11_cl3_0003_iteration() {
         doSingleModelTest("0003-iteration");
+    }
+
+    @Test
+    public void test_11_cl3_0004_lending() {
         doSingleModelTest("0004-lending");
+    }
+
+    @Test
+    public void test_11_cl3_0005_literal_invocation() {
         doSingleModelTest("0005-literal-invocation");
+    }
+
+    @Test
+    public void test_11_cl3_0006_join() {
         doSingleModelTest("0006-join");
+    }
+
+    @Test
+    public void test_11_cl3_0007_date_time() {
         doSingleModelTest("0007-date-time");
+    }
+
+    @Test
+    public void test_11_cl3_0008_listGen() {
         doSingleModelTest("0008-listGen");
+    }
+
+    @Test
+    public void test_11_cl3_0009_append_flatten() {
         doSingleModelTest("0009-append-flatten");
+    }
+
+    @Test
+    public void test_11_cl3_0010_concatenate() {
         doSingleModelTest("0010-concatenate");
+    }
+
+    @Test
+    public void test_11_cl3_0011_insert_remove() {
         doSingleModelTest("0011-insert-remove");
+    }
+
+    @Test
+    public void test_11_cl3_0013_sort() {
         doSingleModelTest("0013-sort");
+    }
+
+    @Test
+    public void test_11_cl3_0014_loan_comparison() {
         doSingleModelTest("0014-loan-comparison");
+    }
+
+    @Test
+    public void test_11_cl3_0016_some_every() {
         doSingleModelTest("0016-some-every");
+    }
+
+    @Test
+    public void test_11_cl3_0017_tableTests() {
         doSingleModelTest("0017-tableTests");
+    }
+
+    @Test
+    public void test_11_cl3_0020_vacation_days() {
         doSingleModelTest("0020-vacation-days");
+    }
+
+    @Test
+    public void test_11_cl3_0021_singleton_list() {
         doSingleModelTest("0021-singleton-list");
+    }
+
+    @Test
+    public void test_11_cl3_0030_user_defined_functions() {
         doSingleModelTest("0030-user-defined-functions");
-        doSingleModelTest("0031-user-defined-functions");
-        doSingleModelTest("0032-conditionals");
-        doSingleModelTest("0033-for-loops");
-        doSingleModelTest("0034-drg-scopes");
-        doSingleModelTest("0035-test-structure-output");
-        doSingleModelTest("0036-dt-variable-input");
-        doSingleModelTest("0037-dt-on-bkm-implicit-params");
-        doSingleModelTest("0038-dt-on-bkm-explicit-params");
-        doSingleModelTest("0039-dt-list-semantics");
-        doSingleModelTest("0040-singlenestedcontext");
-        doSingleModelTest("0041-multiple-nestedcontext");
-        doSingleModelTest("1100-feel-decimal-function");
-        doSingleModelTest("1101-feel-floor-function");
-        doSingleModelTest("1102-feel-ceiling-function");
-        doSingleModelTest("1103-feel-substring-function");
-        doSingleModelTest("1104-feel-string-length-function");
-        doSingleModelTest("1105-feel-upper-case-function");
-        doSingleModelTest("1106-feel-lower-case-function");
-        doSingleModelTest("1107-feel-substring-before-function");
-        doSingleModelTest("1108-feel-substring-after-function");
-        doSingleModelTest("1109-feel-replace-function");
-        doSingleModelTest("1110-feel-contains-function");
-        doSingleModelTest("1115-feel-date-function");
-        doSingleModelTest("1116-feel-time-function");
-        doSingleModelTest("1117-feel-date-and-time-function");
-        doSingleModelTest("1120-feel-duration-function");
-        doSingleModelTest("1121-feel-years-and-months-duration-function");
-
         doSingleModelTest("0030-static-user-defined-functions");
-        doSingleModelTest("0031-static-user-defined-functions");
-        doSingleModelTest("9001-recursive-function");
+    }
 
-        // DMN 1.2 files
+    @Test
+    public void test_11_cl3_0031_user_defined_functions() {
+        doSingleModelTest("0031-user-defined-functions");
+        doSingleModelTest("0031-static-user-defined-functions");
+    }
+
+    @Test
+    public void test_11_cl3_0032_conditionals() {
+        doSingleModelTest("0032-conditionals");
+    }
+
+    @Test
+    public void test_11_cl3_0033_for_loops() {
+        doSingleModelTest("0033-for-loops");
+    }
+
+    @Test
+    public void test_11_cl3_0034_drg_scopes() {
+        doSingleModelTest("0034-drg-scopes");
+    }
+
+    @Test
+    public void test_11_cl3_0035_test_structure_output() {
+        doSingleModelTest("0035-test-structure-output");
+    }
+
+    @Test
+    public void test_11_cl3_0036_dt_variable_input() {
+        doSingleModelTest("0036-dt-variable-input");
+    }
+
+    @Test
+    public void test_11_cl3_0037_dt_on_bkm_implicit_params() {
+        doSingleModelTest("0037-dt-on-bkm-implicit-params");
+    }
+
+    @Test
+    public void test_11_cl3_0038_dt_on_bkm_explicit_params() {
+        doSingleModelTest("0038-dt-on-bkm-explicit-params");
+    }
+
+    @Test
+    public void test_11_cl3_0039_dt_list_semantics() {
+        doSingleModelTest("0039-dt-list-semantics");
+    }
+
+    @Test
+    public void test_11_cl3_0040_singlenestedcontext() {
+        doSingleModelTest("0040-singlenestedcontext");
+    }
+
+    @Test
+    public void test_11_cl3_0041_multiple_nestedcontext() {
+        doSingleModelTest("0041-multiple-nestedcontext");
+    }
+
+    @Test
+    public void test_11_cl3_1100_feel_decimal_function() {
+        doSingleModelTest("1100-feel-decimal-function");
+    }
+
+    @Test
+    public void test_11_cl3_1101_feel_floor_function() {
+        doSingleModelTest("1101-feel-floor-function");
+    }
+
+    @Test
+    public void test_11_cl3_1102_feel_ceiling_function() {
+        doSingleModelTest("1102-feel-ceiling-function");
+    }
+
+    @Test
+    public void test_11_cl3_1103_feel_substring_function() {
+        doSingleModelTest("1103-feel-substring-function");
+    }
+
+    @Test
+    public void test_11_cl3_1104_feel_string_length_function() {
+        doSingleModelTest("1104-feel-string-length-function");
+    }
+
+    @Test
+    public void test_11_cl3_1105_feel_upper_case_function() {
+        doSingleModelTest("1105-feel-upper-case-function");
+    }
+
+    @Test
+    public void test_11_cl3_1106_feel_lower_case_function() {
+        doSingleModelTest("1106-feel-lower-case-function");
+    }
+
+    @Test
+    public void test_11_cl3_1107_feel_substring_before_function() {
+        doSingleModelTest("1107-feel-substring-before-function");
+    }
+
+    @Test
+    public void test_11_cl3_1108_feel_substring_after_function() {
+        doSingleModelTest("1108-feel-substring-after-function");
+    }
+
+    @Test
+    public void test_11_cl3_1109_feel_replace_function() {
+        doSingleModelTest("1109-feel-replace-function");
+    }
+
+    @Test
+    public void test_11_cl3_1110_feel_contains_function() {
+        doSingleModelTest("1110-feel-contains-function");
+    }
+
+    @Test
+    public void test_11_cl3_1115_feel_date_function() {
+        doSingleModelTest("1115-feel-date-function");
+    }
+
+    @Test
+    public void test_11_cl3_1116_feel_time_function() {
+        doSingleModelTest("1116-feel-time-function");
+    }
+
+    @Test
+    public void test_11_cl3_1117_feel_date_and_time_function() {
+        doSingleModelTest("1117-feel-date-and-time-function");
+    }
+
+    @Test
+    public void test_11_cl3_1120_feel_duration_function() {
+        doSingleModelTest("1120-feel-duration-function");
+    }
+
+    @Test
+    public void test_11_cl3_1121_feel_years_and_months_duration_function() {
+        doSingleModelTest("1121-feel-years-and-months-duration-function");
+    }
+
+    @Test
+    public void test_11_cl3_9001_recursive_function() {
+        doSingleModelTest("9001-recursive-function");
+    }
+
+    // DMN 1.2 files
+    @Test
+    public void test_12_cl3_0012_list_functions() {
         doSingleModelTest("0012-list-functions");
+    }
+
+    @Test
+    public void test_12_cl3_0034_drg_scopes() {
+        doSingleModelTest("0034-drg-scopes");
         doSingleModelTest("0034-drg-scopes-2");
+    }
+
+    @Test
+    public void test_12_cl3_0068_feel_equality() {
         doSingleModelTest("0068-feel-equality");
+    }
+
+    @Test
+    public void test_12_cl3_0070_feel_instance_of() {
         doSingleModelTest("0070-feel-instance-of");
+    }
+
+    @Test
+    public void test_12_cl3_0074_feel_properties() {
         doSingleModelTest("0074-feel-properties");
+    }
+
+    @Test
+    public void test_12_cl3_0076_feel_external_java() {
         doSingleModelTest("0076-feel-external-java");
+    }
+
+    @Test
+    public void test_12_cl3_0082_feel_coercion() {
         doSingleModelTest("0082-feel-coercion");
+    }
+
+    @Test
+    public void test_12_cl3_0085_decision_services() {
         doSingleModelTest("0085-decision-services");
+    }
+
+    @Test
+    public void test_12_cl3_0086_import() {
         doMultipleModelsTest(Arrays.asList("0086-import", "0086-imported-model"), new Pair<>("singletonInputData", "false"));
+    }
+
+    @Test
+    public void test_12_cl3_0087_chapter_11_example() {
         doSingleModelTest("0087-chapter-11-example");
+    }
+
+    @Test
+    public void test_12_cl3_0089_nested_inputdata_imports() {
         doMultipleModelsTest(Arrays.asList("0089-nested-inputdata-imports", "0089-model-b", "0089-model-b2", "0089-model-a"), new Pair<>("singletonInputData", "false"));
     }
 }
