7e7e13cb43a9c9621999d4f92877ec1460924ea6
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2021-10-15 17:53:10 +0100

    [#462] Add support for AWS lambdas. Move code in test.

--- dmn-core/src/test/java/com/gs/dmn/transformation/CL3DMNToLambdaTransformerTest.java
+++ dmn-core/src/test/java/com/gs/dmn/transformation/CL3DMNToLambdaTransformerTest.java
@@ -13,17 +13,26 @@
 package com.gs.dmn.transformation;
 
 import com.gs.dmn.dialect.DMNDialectDefinition;
+import com.gs.dmn.dialect.StandardDMNDialectDefinition;
 import com.gs.dmn.log.BuildLogger;
+import com.gs.dmn.log.Slf4jBuildLogger;
+import com.gs.dmn.serialization.DefaultTypeDeserializationConfigurer;
 import com.gs.dmn.serialization.TypeDeserializationConfigurer;
 import com.gs.dmn.transformation.lazy.LazyEvaluationDetector;
+import com.gs.dmn.transformation.lazy.NopLazyEvaluationDetector;
 import com.gs.dmn.transformation.template.TemplateProvider;
+import com.gs.dmn.transformation.template.TreeTemplateProvider;
 import com.gs.dmn.validation.DMNValidator;
+import com.gs.dmn.validation.NopDMNValidator;
 import org.junit.Test;
 import org.omg.dmn.tck.marshaller._20160719.TestCases;
 
 import javax.xml.datatype.Duration;
 import javax.xml.datatype.XMLGregorianCalendar;
+import java.io.File;
 import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CL3DMNToLambdaTransformerTest extends AbstractTckDMNToJavaTransformerTest {
     @Override
@@ -53,5 +62,37 @@ public class CL3DMNToLambdaTransformerTest extends AbstractTckDMNToJavaTransform
     @Test
     public void testCL3() throws Exception {
         doSingleModelTest("1.3","0020-vacation-days");
+    }
+
+    public static void main(String[] args) {
+        // Create transformer
+        StandardDMNDialectDefinition dmnDialect = new StandardDMNDialectDefinition();
+        DMNValidator dmnValidator = new NopDMNValidator();
+        DMNTransformer<TestCases> dmnTransformer = new ToQuotedNameTransformer();
+        TemplateProvider templateProvider = new TreeTemplateProvider();
+        LazyEvaluationDetector lazyEvaluationDetector = new NopLazyEvaluationDetector();
+        TypeDeserializationConfigurer typeDeserializationConfigurer = new DefaultTypeDeserializationConfigurer();
+        InputParameters inputParameters = makeLambdaInputParameters();
+        DMNToLambdaTransformer<BigDecimal, XMLGregorianCalendar, XMLGregorianCalendar, XMLGregorianCalendar, Duration, TestCases> transformer = new DMNToLambdaTransformer<>(
+                dmnDialect, dmnValidator, dmnTransformer, templateProvider, lazyEvaluationDetector, typeDeserializationConfigurer, inputParameters, LOGGER
+        );
+
+        // Transform
+        File inputFileDirectory = new File(STANDARD_FOLDER, "tck/1.3/cl3/0020-vacation-days/0020-vacation-days.dmn");
+        File outputFileDirectory = new File("C:/Work/Projects/aws/bpmn-to-aws-examples/dmn-lambda/");
+        transformer.transformFile(inputFileDirectory, null, outputFileDirectory.toPath());
+    }
+
+    private static InputParameters makeLambdaInputParameters() {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("dmnVersion", "1.3");
+        map.put("modelVersion", "1.1");
+        map.put("platformVersion", "5.0.0");
+
+        map.put("javaRootPackage", "com.gs.lambda");
+        map.put("caching", "true");
+        map.put("singletonDecision", "true");
+
+        return new InputParameters(map);
     }
 }
