0102ba8bb3a1c404214d7085351cefa5e8714598
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2021-11-26 12:28:32 +0000

    [#505] cl3/0098-feel-week-of-year-function: Update the test files.

--- dmn-core/src/test/java/com/gs/dmn/runtime/interpreter/CL3DMNInterpreterTest.java
+++ dmn-core/src/test/java/com/gs/dmn/runtime/interpreter/CL3DMNInterpreterTest.java
@@ -430,7 +430,7 @@ public abstract class CL3DMNInterpreterTest<NUMBER, DATE, TIME, DATE_TIME, DURAT
 
     @Test
     public void test_13_cl3_0098_feel_week_of_year_function() {
-        doSingleModelTest("1.3", "0098-feel-week-of-year-function");
+        doSingleModelTest("1.3", "0098-feel-week-of-year-function", new Pair<>("strongTyping", "false"));
     }
 
     @Test
