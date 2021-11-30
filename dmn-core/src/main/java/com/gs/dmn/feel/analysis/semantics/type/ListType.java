297e5100c05f9226255ae8bc2b84dd9c4b2d0fef
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2021-11-30 16:56:58 +0000

    [#500] cl3/0092-feel-lambda: Replace List<Any> and Any with concrete types.

--- dmn-core/src/main/java/com/gs/dmn/feel/analysis/semantics/type/ListType.java
+++ dmn-core/src/main/java/com/gs/dmn/feel/analysis/semantics/type/ListType.java
@@ -25,6 +25,7 @@ public class ListType extends Type {
     public static final Type DAYS_AND_TIME_DURATION_LIST = new ListType(DurationType.DAYS_AND_TIME_DURATION);
     public static final Type YEARS_AND_MONTHS_DURATION_LIST = new ListType(DurationType.YEARS_AND_MONTHS_DURATION);
     public static final Type CONTEXT_LIST = new ListType(ContextType.ANY_CONTEXT);
+    public static final Type COMPARABLE_LIST = new ListType(ComparableDataType.COMPARABLE);
 
     private final Type elementType;
 
