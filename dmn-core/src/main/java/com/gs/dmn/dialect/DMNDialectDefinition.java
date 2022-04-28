535c22b8c78e5a2adbe2013d5259e41766001a61
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2022-04-28 14:24:14 +0100

    [#530] XStream: Remove deprecated methods from DMNDialectDefinition

--- dmn-core/src/main/java/com/gs/dmn/dialect/DMNDialectDefinition.java
+++ dmn-core/src/main/java/com/gs/dmn/dialect/DMNDialectDefinition.java
@@ -30,8 +30,6 @@ import com.gs.dmn.transformation.lazy.LazyEvaluationDetector;
 import com.gs.dmn.transformation.template.TemplateProvider;
 import com.gs.dmn.validation.DMNValidator;
 
-import java.util.Map;
-
 public interface DMNDialectDefinition<NUMBER, DATE, TIME, DATE_TIME, DURATION, TEST> {
     //
     // Serialization
@@ -43,52 +41,17 @@ public interface DMNDialectDefinition<NUMBER, DATE, TIME, DATE_TIME, DURATION, T
     //
     EnvironmentFactory createEnvironmentFactory();
 
-    /**
-     * @deprecated  Replaced by {@link #createFEELInterpreter(DMNModelRepository, InputParameters)}
-     */
-    @Deprecated
-    default ELInterpreter createFEELInterpreter(DMNModelRepository repository, Map<String, String> inputParameters) {
-        return createFEELInterpreter(repository, new InputParameters(inputParameters));
-    }
     ELInterpreter createFEELInterpreter(DMNModelRepository repository, InputParameters inputParameters);
 
-    /**
-     * @deprecated  Replaced by {@link #createFEELTranslator(DMNModelRepository, InputParameters)}
-     */
-    @Deprecated
-    default ELTranslator createFEELTranslator(DMNModelRepository repository, Map<String, String> inputParameters) {
-        return createFEELTranslator(repository, new InputParameters(inputParameters));
-    }
     ELTranslator createFEELTranslator(DMNModelRepository repository, InputParameters inputParameters);
 
     //
     // DMN Processors
     //
-    /**
-     * @deprecated  Replaced by {@link #createDMNInterpreter(DMNModelRepository, InputParameters)}
-     */
-    @Deprecated
-    default DMNInterpreter<NUMBER, DATE, TIME, DATE_TIME, DURATION> createDMNInterpreter(DMNModelRepository repository, Map<String, String> inputParameters) {
-        return createDMNInterpreter(repository, new InputParameters(inputParameters));
-    }
     DMNInterpreter<NUMBER, DATE, TIME, DATE_TIME, DURATION> createDMNInterpreter(DMNModelRepository repository, InputParameters inputParameters);
 
-    /**
-     * @deprecated  Replaced by {@link #createDMNToNativeTransformer(DMNValidator, DMNTransformer, TemplateProvider, LazyEvaluationDetector, TypeDeserializationConfigurer, InputParameters, BuildLogger)}
-     */
-    @Deprecated
-    default DMNToNativeTransformer createDMNToNativeTransformer(DMNValidator dmnValidator, DMNTransformer<TEST> dmnTransformer, TemplateProvider templateProvider, LazyEvaluationDetector lazyEvaluationDetector, TypeDeserializationConfigurer typeDeserializationConfigurer, Map<String, String> inputParameters, BuildLogger logger) {
-        return createDMNToNativeTransformer(dmnValidator, dmnTransformer, templateProvider, lazyEvaluationDetector, typeDeserializationConfigurer, new InputParameters(inputParameters), logger);
-    }
     DMNToNativeTransformer createDMNToNativeTransformer(DMNValidator dmnValidator, DMNTransformer<TEST> dmnTransformer, TemplateProvider templateProvider, LazyEvaluationDetector lazyEvaluationDetector, TypeDeserializationConfigurer typeDeserializationConfigurer, InputParameters inputParameters, BuildLogger logger);
 
-    /**
-     * @deprecated  Replaced by {@link #createBasicTransformer(DMNModelRepository, LazyEvaluationDetector, InputParameters)}
-     */
-    @Deprecated
-    default BasicDMNToJavaTransformer createBasicTransformer(DMNModelRepository repository, LazyEvaluationDetector lazyEvaluationDetector, Map<String, String> inputParameters) {
-        return createBasicTransformer(repository, lazyEvaluationDetector, new InputParameters(inputParameters));
-    }
     BasicDMNToJavaTransformer createBasicTransformer(DMNModelRepository repository, LazyEvaluationDetector lazyEvaluationDetector, InputParameters inputParameters);
 
     //
