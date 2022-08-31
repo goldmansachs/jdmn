a959be3600bcc7eb5306f7fc1d0a12f924a92f3a
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2022-08-31 17:40:30 +0100

    [#557] DMN to Python: Add TCK integration tests for Python

---
+++ dmn-tck-it/dmn-tck-it-python-translator/ci/make_env.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+set CI_PROJECT_DIR=%cd%
+
+if "%1"=="" (
+    echo Usage make_env.bat PYTHON_ENV_DIR
+    exit /b 1
+)
+
+@REM set PIP_INDEX_URL=https://pypi.python.org/pypi
+@REM set PIP_TRUSTED_HOST=pypi.python.org
+SET PIP_EXTRA_INDEX_URL=https://test.pypi.org/pypi
+
+set PYTHON_ENV_DIR=%1
+python -m venv %PYTHON_ENV_DIR%
+call %PYTHON_ENV_DIR%/Scripts/activate
+
+python --version
+pip --version
+python -m pip install -U pip
+pip install -r %CI_PROJECT_DIR%/ci/ci_requirements.txt
---
+++ dmn-tck-it/dmn-tck-it-python-translator/ci/make_env.sh
@@ -0,0 +1,18 @@
+if [ -z "$1" ]
+then
+    echo "Usage make_env.bat PYTHON_ENV_DIR"
+    exit 1
+fi
+
+# export PIP_INDEX_URL='https://pypi.python.org/pypi'
+# export PIP_TRUSTED_HOST='pypi.python.org'
+export PIP_EXTRA_INDEX_URL='https://test.pypi.org/pypi'
+
+export PYTHON_ENV_DIR=%1
+python -m venv $PYTHON_ENV_DIR
+source $PYTHON_ENV_DIR/bin/activate
+
+python --version
+pip --version
+python -m pip install -U pip
+pip install -r $CI_PROJECT_DIR/ci/ci_requirements.txt
