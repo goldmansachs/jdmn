06bf869ad63064a16d89da05e513a9c5e8849ab7
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2024-07-22 15:19:16 +0100

    [#690] Test coverage: Increase test coverage for NopVisitor.

--- dmn-core/src/test/java/com/gs/dmn/ast/visitor/NopVisitorTest.java
+++ dmn-core/src/test/java/com/gs/dmn/ast/visitor/NopVisitorTest.java
@@ -12,8 +12,8 @@
  */
 package com.gs.dmn.ast.visitor;
 
-import com.gs.dmn.ast.TDefinitions;
-import com.gs.dmn.ast.Visitor;
+import com.gs.dmn.ast.*;
+import com.gs.dmn.ast.dmndi.*;
 import com.gs.dmn.error.NopErrorHandler;
 import com.gs.dmn.log.NopBuildLogger;
 import com.gs.dmn.serialization.DMNMarshaller;
@@ -43,5 +43,295 @@ public class NopVisitorTest {
         TDefinitions definitions = marshaller.unmarshal(new FileReader(inputXMLFile), true);
         Object result = definitions.accept(visitor, null);
         assertNull(result);
+    }
+
+    @Test
+    public void testVisitTDefinitions() {
+        assertNull(visitor.visit((TDefinitions) null, null));
+    }
+
+    @Test
+    public void testVisitTImport() {
+        assertNull(visitor.visit((TImport) null, null));
+    }
+
+    @Test
+    public void testVisitTImportedValues() {
+        assertNull(visitor.visit((TImportedValues) null, null));
+    }
+
+    @Test
+    public void testVisitTItemDefinition() {
+        assertNull(visitor.visit((TItemDefinition) null, null));
+    }
+
+    @Test
+    public void testVisitTFunctionItem() {
+        assertNull(visitor.visit((TFunctionItem) null, null));
+    }
+
+    @Test
+    public void testVisitTInputData() {
+        assertNull(visitor.visit((TInputData) null, null));
+    }
+
+    @Test
+    public void testVisitTDecision() {
+        assertNull(visitor.visit((TDecision) null, null));
+    }
+
+    @Test
+    public void testVisitTBusinessKnowledgeModel() {
+        assertNull(visitor.visit((TBusinessKnowledgeModel) null, null));
+    }
+
+    @Test
+    public void testVisitTDecisionService() {
+        assertNull(visitor.visit((TDecisionService) null, null));
+    }
+
+    @Test
+    public void testVisitTKnowledgeSource() {
+        assertNull(visitor.visit((TKnowledgeSource) null, null));
+    }
+
+    @Test
+    public void testVisitTContext() {
+        assertNull(visitor.visit((TContext) null, null));
+    }
+
+    @Test
+    public void testVisitTContextEntry() {
+        assertNull(visitor.visit((TContextEntry) null, null));
+    }
+
+    @Test
+    public void testVisitTDecisionTable() {
+        assertNull(visitor.visit((TDecisionTable) null, null));
+    }
+
+    @Test
+    public void testVisitTInputClause() {
+        assertNull(visitor.visit((TInputClause) null, null));
+    }
+
+    @Test
+    public void testVisitTOutputClause() {
+        assertNull(visitor.visit((TOutputClause) null, null));
+    }
+
+    @Test
+    public void testVisitTRuleAnnotationClause() {
+        assertNull(visitor.visit((TRuleAnnotationClause) null, null));
+    }
+
+    @Test
+    public void testVisitTDecisionRule() {
+        assertNull(visitor.visit((TDecisionRule) null, null));
+    }
+
+    @Test
+    public void testVisitTRuleAnnotation() {
+        assertNull(visitor.visit((TRuleAnnotation) null, null));
+    }
+
+    @Test
+    public void testVisitTFunctionDefinition() {
+        assertNull(visitor.visit((TFunctionDefinition) null, null));
+    }
+
+    @Test
+    public void testVisitTInvocation() {
+        assertNull(visitor.visit((TInvocation) null, null));
+    }
+
+    @Test
+    public void testVisitTBinding() {
+        assertNull(visitor.visit((TBinding) null, null));
+    }
+
+    @Test
+    public void testVisitTList() {
+        assertNull(visitor.visit((TList) null, null));
+    }
+
+    @Test
+    public void testVisitTLiteralExpression() {
+        assertNull(visitor.visit((TLiteralExpression) null, null));
+    }
+
+    @Test
+    public void testVisitTRelation() {
+        assertNull(visitor.visit((TRelation) null, null));
+    }
+
+    @Test
+    public void testVisitTUnaryTests() {
+        assertNull(visitor.visit((TUnaryTests) null, null));
+    }
+
+    @Test
+    public void testVisitTConditional() {
+        assertNull(visitor.visit((TConditional) null, null));
+    }
+
+    @Test
+    public void testVisitTFor() {
+        assertNull(visitor.visit((TFor) null, null));
+    }
+
+    @Test
+    public void testVisitTFilter() {
+        assertNull(visitor.visit((TFilter) null, null));
+    }
+
+    @Test
+    public void testVisitTEvery() {
+        assertNull(visitor.visit((TEvery) null, null));
+    }
+
+    @Test
+    public void testVisitTSome() {
+        assertNull(visitor.visit((TSome) null, null));
+    }
+
+    @Test
+    public void testVisitTChildExpression() {
+        assertNull(visitor.visit((TChildExpression) null, null));
+    }
+
+    @Test
+    public void testVisitTTypedChildExpression() {
+        assertNull(visitor.visit((TTypedChildExpression) null, null));
+    }
+
+    @Test
+    public void testVisitTAuthorityRequirement() {
+        assertNull(visitor.visit((TAuthorityRequirement) null, null));
+    }
+
+    @Test
+    public void testVisitTInformationRequirement() {
+        assertNull(visitor.visit((TInformationRequirement) null, null));
+    }
+
+    @Test
+    public void testVisitTKnowledgeRequirement() {
+        assertNull(visitor.visit((TKnowledgeRequirement) null, null));
+    }
+
+    @Test
+    public void testVisitTInformationItem() {
+        assertNull(visitor.visit((TInformationItem) null, null));
+    }
+
+    @Test
+    public void testVisitTDMNElementReference() {
+        assertNull(visitor.visit((TDMNElementReference) null, null));
+    }
+
+    @Test
+    public void testVisitTAssociation() {
+        assertNull(visitor.visit((TAssociation) null, null));
+    }
+
+    @Test
+    public void testVisitTGroup() {
+        assertNull(visitor.visit((TGroup) null, null));
+    }
+
+    @Test
+    public void testVisitTTextAnnotation() {
+        assertNull(visitor.visit((TTextAnnotation) null, null));
+    }
+
+    @Test
+    public void testVisitTBusinessContextElement() {
+        assertNull(visitor.visit((TBusinessContextElement) null, null));
+    }
+
+    @Test
+    public void testVisitTPerformanceIndicator() {
+        assertNull(visitor.visit((TPerformanceIndicator) null, null));
+    }
+
+    @Test
+    public void testVisitTOrganizationUnit() {
+        assertNull(visitor.visit((TOrganizationUnit) null, null));
+    }
+
+    @Test
+    public void testVisitTElementCollection() {
+        assertNull(visitor.visit((TElementCollection) null, null));
+    }
+
+    @Test
+    public void testVisitTDMNElementExtensionElement() {
+        assertNull(visitor.visit((TDMNElement.ExtensionElements) null, null));
+    }
+
+    @Test
+    public void testVisitDMNDI() {
+        assertNull(visitor.visit((DMNDI) null, null));
+    }
+
+    @Test
+    public void testVisitDMNDiagram() {
+        assertNull(visitor.visit((DMNDiagram) null, null));
+    }
+
+    @Test
+    public void testVisitDMNShape() {
+        assertNull(visitor.visit((DMNShape) null, null));
+    }
+
+    @Test
+    public void testVisitDMNEdge() {
+        assertNull(visitor.visit((DMNEdge) null, null));
+    }
+
+    @Test
+    public void testVisitDMNStyle() {
+        assertNull(visitor.visit((DMNStyle) null, null));
+    }
+
+    @Test
+    public void testVisitDMNLabel() {
+        assertNull(visitor.visit((DMNLabel) null, null));
+    }
+
+    @Test
+    public void testVisitDMNDecisionServiceDividerLine() {
+        assertNull(visitor.visit((DMNDecisionServiceDividerLine) null, null));
+    }
+
+    @Test
+    public void testVisitColor() {
+        assertNull(visitor.visit((Color) null, null));
+    }
+
+    @Test
+    public void testVisitPoint() {
+        assertNull(visitor.visit((Point) null, null));
+    }
+
+    @Test
+    public void testVisitBounds() {
+        assertNull(visitor.visit((Bounds) null, null));
+    }
+
+    @Test
+    public void testVisitDimension() {
+        assertNull(visitor.visit((Dimension) null, null));
+    }
+
+    @Test
+    public void testVisitDiagramElementExtension() {
+        assertNull(visitor.visit((DiagramElement.Extension) null, null));
+    }
+
+    @Test
+    public void testVisitStyleExtension() {
+        assertNull(visitor.visit((Style.Extension) null, null));
     }
 }
