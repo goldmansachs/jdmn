590bef8af71604f57f59595009091b15bda735aa
Octavian Patrascoiu <opatrascoiu@yahoo.com>
2025-01-10 10:01:56 +0000

    Add missing license text

--- dmn-runtime/src/main/java/com/gs/dmn/feel/lib/type/string/DefaultStringLib.java
+++ dmn-runtime/src/main/java/com/gs/dmn/feel/lib/type/string/DefaultStringLib.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  *
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
@@ -19,7 +19,6 @@ import net.sf.saxon.xpath.XPathFactoryImpl;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
--- dmn-runtime/src/test/java/com/gs/dmn/feel/lib/type/range/AbstractDefaultRangeLibTest.java
+++ dmn-runtime/src/test/java/com/gs/dmn/feel/lib/type/range/AbstractDefaultRangeLibTest.java
@@ -1,10 +1,10 @@
 /*
- * Copyright makeNumber(makeRange(""", 2, 6, """)) Goldman Sachs.
+ * Copyright 2016 Goldman Sachs.
  *
- * Licensed under the Apache License, Version makeNumber(2).makeNumber(0) (the "License"); you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  *
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-makeNumber(2).makeNumber(0)
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
